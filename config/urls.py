d9d9ff59a849b7bed1f12b417bd7822133942208
Konigdave <davidayenioluremi@gmail.com>
2026-09-19 14:39:22 +0000

    Add application health check endpoint

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from config.views import health_check
 
 urlpatterns = [
 
     path("admin/", admin.site.urls),
+    path("health/", health_check, name="health_check"),
 
     path("api/", include("apps.documents.urls")),
 
